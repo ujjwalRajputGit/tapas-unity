140f7d66ff09add727a15ecaea3302230088044d
ujjwalRajputGit <ujjwalRajputGit@users.noreply.github.com>
2021-05-19 21:09:59 +0530

    alphabets showing randomly

--- Assets/Script/AbcMatchingController.cs
+++ Assets/Script/AbcMatchingController.cs
@@ -5,10 +5,11 @@ using TMPro;
 
 public class AbcMatchingController : MonoBehaviour
 {
-  [SerializeField]List<TMP_Text> upperText;
+  [SerializeField]List<TMP_Text> letterTexts;
+  [SerializeField]List<TMP_Text> alphabetTexts;
   List<string> letterList = new List<string>();
-  List<string> wordList = new List<string>(new string[] 
-  { 
+  List<string> alphabetList = new List<string>();
+  List<string> wordCollectinList = new List<string>(new string[] { 
     "Apple", "Ant", "Axe",
     "Ball", "Boy", "Book",
     "Cat", "Clock", "Cake",
@@ -36,31 +37,56 @@ public class AbcMatchingController : MonoBehaviour
     "Yellow", "Yak", "Yoga",
     "Zebra", "Zero", "Zoo"
   });
+  
   private void Start() {
-    UpperTextDisplay();
+    DisplayLettes();
+    DisplayAlphabets();
   }
-  void UpperTextDisplay() { 
+  void DisplayLettes() { 
     letterList.Clear(); 
-    while (letterList.Count < upperText.Count) {
+    while (letterList.Count < letterTexts.Count) {
       string letter = RandomLetterGenerator();
       if(!letterList.Contains(letter)) {
-        letterList.Add(letter); 
+        letterTexts[letterList.Count].text = letter;
+        letterList.Add(letter);
       }            
-    } 
-    for(int i = 0; i < upperText.Count; i++) {
-      upperText[i].text = letterList[i].ToUpper();
-    }   
+    }
   }
-    //    GetComponent<TMP_Text>().text = randomLetter.ToString().ToUpper();
+
+  void DisplayAlphabets() {
+    LettersMatchingAlphabetGenerator();
+    foreach (TMP_Text t in alphabetTexts) {
+      int random = Random.Range(0, alphabetList.Count);
+      t.text = alphabetList[random];
+      alphabetList.RemoveAt(random);
+    }
+  }
+
+  void LettersMatchingAlphabetGenerator() {
+    alphabetList.Clear();
+    foreach (string s in letterList) {
+      int r1 = (StringToInt(s,0) - 65) * 3 ;
+      int r2 = (3 + r1);
+      int random = Random.Range(r1, r2);
+      alphabetList.Add(wordCollectinList[random]);
+    }
+  }
+  // GetComponent<TMP_Text>().text = randomLetter.ToString().ToUpper();
+
+  int StringToInt(string s, int index) {
+    int i = s[index];
+    return i;
+  }
+    
   string RandomLetterGenerator() {
     int randomNum = Random.Range(0,26);
-    char randomLetter = (char) ('a' + randomNum);
+    char randomLetter = (char) ('A' + randomNum);
     return randomLetter.ToString();
   }
 
   private void Update() {
     if(Input.GetKeyDown(KeyCode.P)) {
-      Debug.Log((char) ('a' + 1));
+      Debug.Log(StringToInt("A",0));
     }
         
   }
