ef6c8f67e24f568e843560829c59b2af012a7812
TomarNikhil <nikku4665@gmail.com>
2021-07-04 12:34:12 +0530

    Bubble Burst logic Completed

--- Assets/Script/BubbleBurst/GameController.cs
+++ Assets/Script/BubbleBurst/GameController.cs
@@ -7,6 +7,8 @@ namespace Tapas.BubbleBurst
 	public class GameController : MonoBehaviour
 	{
 		[SerializeField] List<TMP_Text> bubbleTexts;
+		[SerializeField] TMP_Text alphabetText;
+		public string randomAlphabet;
 		List<string> wordList = new List<string>();
 		List<string> wordCollectionList = new List<string>(new[] {
 				"Apple", "Ant", "Axe",
@@ -38,27 +40,51 @@ namespace Tapas.BubbleBurst
 		});
 
 		private void Start() {
-			DisplayWords();
+			DisplayAlphabet();
+			DisplayWords(); 
 		}
 
-		void DisplayWords() {
+    private void DisplayAlphabet()
+    {
+      
+			randomAlphabet = RandomAlphabetGenerator();
+			alphabetText.text = randomAlphabet;
+		}
+
+		string RandomAlphabetGenerator() {
+			int randomNum = Random.Range(0, 26);
+			char randomLetter = (char) ('A' + randomNum);
+			return randomLetter.ToString();
+		}
+
+    void DisplayWords() {
 			AlphabetSimilarWordsGenerator();
 			foreach (TMP_Text t in bubbleTexts) {
 				int random = Random.Range(0, wordList.Count);
 				t.text = wordList[random];
 				wordList.RemoveAt(random);
-			}
+			 }
 		}
-
+    int RandomNumberExcept(int value) 
+    {
+       int num = Random.Range(0,78);
+      if(num == value || num == value + 1 || num == value+ 2) {
+      num = RandomNumberExcept(value);
+    }
+      return num;
+   }
 		void AlphabetSimilarWordsGenerator() {
 			wordList.Clear();
-			int r1 = (StringToInt("A", 0) - 65) * 3;
+			int r1 = (StringToInt(randomAlphabet, 0) - 65) * 3;
 			wordList.Add(wordCollectionList[r1]);
 			wordList.Add(wordCollectionList[r1 + 1]);
 			wordList.Add(wordCollectionList[r1 + 2]);
-		}
+			wordList.Add(wordCollectionList[RandomNumberExcept(r1)]);
+			wordList.Add(wordCollectionList[RandomNumberExcept(r1)]);
+			}
 
-		private int StringToInt(string s, int index) {
+		private int StringToInt(string s, int index)
+		{
 			int i = s[index];
 			return i;
 		}
